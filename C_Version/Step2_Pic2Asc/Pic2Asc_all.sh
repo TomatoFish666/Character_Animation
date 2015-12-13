43ac7fbd20c97df73935e6bd501d6ae4a04c2ab2
TomatoFishs <tomatofishs@gmail.com>
2015-12-14 03:08:46 +0800

    Pic2Asc_all.sh finish

---
+++ C_Version/Step2_Pic2Asc/Pic2Asc_all.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+function Usage(){
+    echo ""
+    echo "Usage:"
+    echo "./Pic2Asc_all.sh dirName targetFileName"
+    echo ""
+    echo "dirName is the directory which contains all the pictures you want to transform."
+    echo "targetFileName is the text file which save the result(ASCII characters)."
+    echo ""
+}
+
+if [ $# -lt "2" ] ; then
+    Usage
+    exit 0
+fi
+
+for eachPic in `ls $1`
+do
+    ./test $eachPic $2
+done
